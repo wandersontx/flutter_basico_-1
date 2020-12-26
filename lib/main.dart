af48c627aecc4c163ca819f22a215089a22306b6
wandersontx <wandersonh89@gmail.com>
2020-12-26 11:06:48 -0300

    Espaçamentos

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false, //para não exibir a faixa de status debug
     title: "Frases do dia2 ",
     home: Container(
-      color: Colors.white,
+      padding: EdgeInsets.fromLTRB(10, 15, 10, 15),
+      margin: EdgeInsets.fromLTRB(20, 25, 20, 25),
+      decoration:
+          BoxDecoration(border: Border.all(width: 3, color: Colors.white)),
       child: Column(
         children: [
-          FlatButton(
-              onPressed: () {
-                print("Botão pressionado!");
-              },
-              child: Text(
-                "Clique aqui",
-                style: TextStyle(
-                    fontSize: 20,
-                    color: Colors.black,
-                    decoration: TextDecoration.none),
-              ))
+          Text("Texto 1"),
+          Padding(
+            padding: EdgeInsets.only(top: 20, bottom: 40),
+            child: Text("Texto 2"),
+          ),
+          Text("Texto 3"),
         ],
       ),
     ),
